727cb7a4ec0c20404bc93e1286780678aea047c8
ICB Yuge Ji <jiyuge@gmail.com>
2021-01-24 23:21:16 +0100

    proper table from Svensson's db

---
+++ readme_body.txt
@@ -0,0 +1,9 @@
+# sc-pert
+
+Modeling objectives, modeling approaches, and datasets for perturbational single-cell omics.
+
+* [Table of perturbation models](https://docs.google.com/spreadsheets/d/1nqNg0DW1-Om7WtvRS20q-6b28usVRv5czOcxgj83Sgg/)
+* List of perturbation datasets, curated from [Svensson et. al (2020)](https://doi.org/10.1093/database/baaa073)
+
+
+
---
+++ update.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+
+# re-download data
+os.system('rm data.tsv')
+os.system('wget http://www.nxn.se/single-cell-studies/data.tsv')
+
+dois = [
+### CRISPR ###
+    '10.1016/j.cell.2016.11.038',  # Dixit 2016
+    '10.1016/j.cell.2016.11.048',  # Adamson 2016
+    '10.1126/science.1247651',     # Jaitin 2016
+    '10.1038/nmeth.4177',          # Datlinger 2017
+    # Hill 2018
+    '10.1016/j.cell.2018.11.029',  # Gasperini 2019
+    # Ursu 2020
+### small molecules ### 
+    '10.1126/science.aax6234',     # Srivatsan 2019 
+    '10.1101/868752',              # McFarland 2020
+    # PhEMD dataset (Chen 2020)
+    # Zhao 2020
+]
+
+df = pd.read_csv('data.tsv', sep='\t')
+df = df[df.DOI.isin(dois)]
+
+# convert DOIs to links in markdown
+links = []
+for shorthand, link in df[['Shorthand', 'DOI']].values:
+    links.append(f'[{shorthand}](doi.org/{link})')
+df['Shorthand'] = links
+df = df.drop(['Authors', 'Journal', 'DOI', 'bioRxiv DOI'], axis=1)
+
+# write README
+filenames = []
+with open('README.md', 'w') as outfile:
+    with open('readme_body.txt') as infile:
+        outfile.write(infile.read())
+        md = df.to_markdown(index=False, tablefmt='github')
+        md = md.replace('| Title', '| Title'+'&nbsp;'*100)
+        outfile.write(md)
+        infile.close()
+    outfile.close()
