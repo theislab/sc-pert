d66cb7cccb41dd6574fe80ab03d59c29957e8ded
ICB Yuge Ji <jiyuge@gmail.com>
2021-06-15 11:02:58 +0200

    up-to-date dataset list

--- readme_body.txt
+++ readme_body.txt
@@ -1,9 +1,9 @@
-# sc-pert
+# sc-pert - Machine learning for perturbational single-cell omics
 
-Modeling objectives, modeling approaches, and datasets for perturbational single-cell omics.
+*This repository provides a community-maintained summary of models and datasets. It was initially curated for [(Cell Systems, 2021)](https://doi.org/10.1016/j.cels.2021.05.016).*
 
-* List of perturbation models, [curated at scrna-tools](https://www.scrna-tools.org/tools?sort=name&cats=Perturbations). For additional details included in the perspective, see [table of perturbation models](https://docs.google.com/spreadsheets/d/1nqNg0DW1-Om7WtvRS20q-6b28usVRv5czOcxgj83Sgg/)
-* List of perturbation datasets, curated from [Svensson et. al (2020)](https://doi.org/10.1093/database/baaa073)
+We [curated a list of perturbation-related tools at scrna-tools](https://www.scrna-tools.org/tools?sort=name&cats=Perturbations). Please consider further updating and tagging tools [there](https://github.com/scRNA-tools/scRNA-tools).
 
+This [spreadsheet of a subset of perturbation models](https://docs.google.com/spreadsheets/d/1nqNg0DW1-Om7WtvRS20q-6b28usVRv5czOcxgj83Sgg/) includes more details, and is the basis of the table in the article.
 
-
+Below, we curated a table of perturbation datasets based on [Svensson *et al.* (2020)](https://doi.org/10.1093/database/baaa073).
--- update.py
+++ update.py
@@ -16,14 +16,21 @@ dois = [
     '10.1016/j.cell.2016.11.048',  # Adamson 2016
     '10.1126/science.1247651',     # Jaitin 2016
     '10.1038/nmeth.4177',          # Datlinger 2017
-    # Hill 2018
+    '10.1038/nmeth.4604',          # Hill 2018
     '10.1016/j.cell.2018.11.029',  # Gasperini 2019
-    # Ursu 2020
+    # Norman 2019
+    '10.1038/s41592-020-0837-5',   # Schraivogel 2020
+    # Josh 2020
+    '10.1101/2020.11.16.383307',   # Ursu 2020
+    '10.1101/791525',              # Jin 2020
+    '10.1038/s41588-021-00779-1',  # Frangieh 2021
 ### small molecules ### 
-    '10.1126/science.aax6234',     # Srivatsan 2019 
+    '10.1126/science.aax6234',     # Srivatsan 2019
+    '10.1126/sciadv.aav2249',      # Shin 2019
     '10.1101/868752',              # McFarland 2020
-    # PhEMD dataset (Chen 2020)
-    # Zhao 2020
+    # PhEMD dataset (Chen 2020) - cytobank
+    '10.1101/2020.04.22.056341',   # Zhao 2020
+    '10.1038/s41592-021-01153-z',  # Datlinger 2021
 ]
 
 df = pd.read_csv('data.tsv', sep='\t')
@@ -32,7 +39,9 @@ df = df[df.DOI.isin(dois)]
 # convert DOIs to links in markdown
 links = []
 for shorthand, link in df[['Shorthand', 'DOI']].values:
-    links.append(f'[{shorthand}](doi.org/{link})')
+    s = f'[{shorthand}](doi.org/{link})'
+    s = s.replace('et al', '*et al.*')
+    links.append(s)
 df['Shorthand'] = links
 df = df.drop(['Authors', 'Journal', 'DOI', 'bioRxiv DOI'], axis=1)
 
