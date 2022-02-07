11e0ef11c417ad69048339b354b5b567a890016d
ICB Yuge Ji <jiyuge@gmail.com>
2022-02-07 23:26:26 +0100

    template nb and block edits

--- datasets/block1_init.py
+++ datasets/block1_init.py
@@ -1,14 +1,18 @@
-adata = sc.read(f'{author_year}_raw.h5ad')
-
 # metalabels
-adata.uns['preprocessing_nb_link'] = 'https://nbviewer.org/github/theislab/sc-pert/blob/main/datasets/{author_year}_curation.ipynb'
+adata.uns['preprocessing_nb_link'] = f'https://nbviewer.org/github/theislab/sc-pert/blob/main/datasets/{author_year}_curation.ipynb'
 adata.uns['doi'] = doi
-print(adata)
 display(adata.obs.describe(include='all').T.head(20))
+
+# use gene symbols as gene names
+if var_genes:
+    adata.var[var_genes] = adata.var[var_genes].astype(str)
+    adata.var = adata.var.reset_index().set_index(var_genes)
+    print(adata.var_names)
 
 # filtering and processing
 sc.pp.filter_cells(adata, min_genes=200)
 sc.pp.filter_genes(adata, min_cells=20)
+adata.var_names_make_unique()
 
 adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
 sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
@@ -16,10 +20,3 @@ sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False,
 sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'], jitter=0.4, multi_panel=True)
 sc.pl.scatter(adata, x='total_counts', y='pct_counts_mt')
 sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts')
-
-# use gene symbols as gene names
-if var_genes:
-    adata.var.reset_index().set_index(var_genes)
-    adata.var_names = adata.var.index
-
-adata.var_names_make_unique()
