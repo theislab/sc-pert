b919652c3f3a6e188aa843663146e52e927e1394
ICB Yuge Ji <jiyuge@gmail.com>
2022-02-10 04:30:38 +0100

    fix control labeling

--- datasets/block3_standardize.py
+++ datasets/block3_standardize.py
@@ -1,5 +1,9 @@
 # the following fields are meant to serve as a template
-adata.obs['perturbation_name'] = ???
+control = ???
+replace_dict = {
+    control: 'control',
+}
+adata.obs['perturbation_name'] = adata.obs[???].replace(replace_dict)
 adata.obs['perturbation_type'] = # 'small molecule' or 'genetic'
 adata.obs['perturbation_value'] = adata.obs[???]
 adata.obs['perturbation_unit'] = # 'ug', 'mg', 'hrs', etc.
